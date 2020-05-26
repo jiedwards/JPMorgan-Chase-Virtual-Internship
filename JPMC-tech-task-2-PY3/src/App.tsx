df8950d239d936e8eda61e2a59a80b173947a9fe
jiedwards <jacobedwards1995@hotmail.co.uk>
2020-05-26 14:46:05 +0100

    added required changes for task 2

--- JPMC-tech-task-2-PY3/src/App.tsx
+++ JPMC-tech-task-2-PY3/src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +23,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      //Only show graph when user toggles data stream button
+      showGraph: false,
     };
   }
 
@@ -29,18 +32,31 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data} />)
+    }
   }
 
   /**
-   * Get new data from server and update the state with the new data
+   * Get new data from server and update the state with the new data using an interval timer
+   * to ensure that the polling is consistent and is not waiting for the user to continually request the data.
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let dataRetrievalTimer = 0;
+    const dataRetrievalInterval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      dataRetrievalTimer++;
+      if (dataRetrievalTimer > 1000) {
+        clearInterval(dataRetrievalInterval);
+      }
+    }, 100);
   }
 
   /**
@@ -59,7 +75,7 @@ class App extends Component<{}, IState> {
             // As part of your task, update the getDataFromServer() function
             // to keep requesting the data every 100ms until the app is closed
             // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
+            onClick={() => { this.getDataFromServer() }}>
             Start Streaming Data
           </button>
           <div className="Graph">
